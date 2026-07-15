1b8c35cdbdbdef847185192f9334f3b0a1604c94
Noor <nooressamjx2005@gmail.com>
2026-07-15 19:38:34 +0300

    Restructure repository: Remove backend and move frontend to root

--- Backend/src/category/category.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface category {
-    id: string;
-    name: string;
-    photo: string;
-    slug: string;
-    created_at: string;
-}
--- Backend/src/routineassembler/skinProfile.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface SkinProfile {
-    id: string;
-    created_at: string;
-    user_id: string | null;
-    skin_type: string;
-    concern: string[];
-    sensitivity: string;
-}
